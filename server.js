d976a9ccfe865a36b3bd97d3d741ad7ebac4db01
Cosmin C <concosminx@users.noreply.github.com>
2021-12-27 18:42:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const bcrypt = require('bcryptjs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require("./controllers/register");
+const signin = require("./controllers/signin");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
 
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//         host: '127.0.0.1',
-//         user: 'postgres',
-//         password: 'postgres',
-//         database: 'smartbrain'
-//     }
-// });
-
-const db = knex({
-    client: 'pg',
-    connection: {
+//use local db or production ...
+const db = process.env.DATABASE_URL
+  ? knex({
+      client: "pg",
+      connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: { rejectUnauthorized: false }
-    }
-});
-
+        ssl: { rejectUnauthorized: false },
+      },
+    })
+  : knex({
+      client: "pg",
+      connection: {
+        host: "127.0.0.1",
+        user: "postgres",
+        password: "postgres",
+        database: "smartbrain",
+      },
+    });
 
 const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(cors());
 
-
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfileGet(req, res, db);
+});
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`app is running on port ${port}`);
+  console.log(`app is running on port ${port}`);
 });
 
 //windows, set env variable: $env:PORT="2020"
