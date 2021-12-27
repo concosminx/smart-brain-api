aeb0d90f10d41e2eb76a949c9d4479d8f249d6d3
Cosmin C <concosminx@users.noreply.github.com>
2021-12-27 16:06:08 +0200

    my own project from zero + removed api key

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
-const bcrypt = require('bcrypt-nodejs');
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
 const knex = require('knex');
 
@@ -10,28 +10,31 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  // connect to your own database here:
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'postgres',
+        database: 'smartbrain'
+    }
 });
+
 
 const app = express();
 
-app.use(cors())
-app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+app.use(bodyParser.json())
+app.use(cors());
 
-app.get('/', (req, res)=> { res.send(db.users) })
+
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`app is running on port ${port}`);
+});
+
+//windows, set env variable: $env:PORT="2020"
